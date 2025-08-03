300576a3988705182e08dec14f0de8441c272fee
Maxim Matveev <maxx.matveev@gmail.com>
2025-08-04 01:33:37 +0300

    [BugFix] [Feature] Underline for Settings
    - Реализовано подчеркивание ссылки "Настройки" при переходе на страницу "Настройки"

--- src/apps/web/pomodoro-web-app/src/components/header/HeaderSettings/HeaderSettings.tsx
+++ src/apps/web/pomodoro-web-app/src/components/header/HeaderSettings/HeaderSettings.tsx
@@ -1,17 +1,24 @@
+"use client"
+
 import * as React from 'react';
 import Link from 'next/link';
 import {cn} from "@/libs/utils";
 import { HiCog } from "react-icons/hi";
+import {usePathname} from "next/navigation";
 
 type Props = {
 
 };
 
 const HeaderSettings = (props: Props) => {
+    const pathname = usePathname();
+    const isActive = pathname === '/settings';
+
     return (
-        <Link href={"/settings"} className={cn("flex items-center gap-2 text-gray-600")}>
+        <Link href={"/settings"} className={cn("flex items-center gap-2",
+                isActive ? 'text-gray-900' : 'text-gray-600')}>
             <HiCog size={24} />
-            <span>Настройки</span>
+            <span className={cn(isActive ? 'border-b-1 border-b-[#800020]' : "")}>Настройки</span>
         </Link>
     );
 };
